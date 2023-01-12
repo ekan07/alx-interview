69de2a6673cc96a3ecdf0e4e9463a75b3c605b4f
ekan07 <aniekanbon@gmail.com>
2023-01-12 21:06:20 +0100

    Update file: 0-minoperations.py

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,14 +1,26 @@
 #!/usr/bin/python3
-"""
-0. Minimum Operations
+"""calculates the fewest number of operations needed to result
+    in exactly n H characters in the file
 """
 
 
-def minOperations(n):
-    d = n // 2
-    while d > 0:
-        q, r = divmod(n, d)
-        if (r == 0):
-            return q + minOperations(d)
-        d -= 1
-    return 0
+def minOperations(n: int) -> int:
+    """Minimum operation"""
+    if not isinstance(n, int) or n < 2:
+        return 0
+
+    prev = 1
+    operations = cur = value = 2
+
+    while n > value:
+        reminder = n - value
+        if reminder % cur == 0:
+            operations += 2
+            prev = cur
+            value += cur
+            cur = value
+        elif reminder % prev == 0:
+            operations += 1
+            value += prev
+            cur = value
+    return operations
